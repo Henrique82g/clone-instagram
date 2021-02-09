59862417b988511fd0a3b5fbf8fcc80d398e2ec7
Henrique Gonçalves <henriqueco504@gmail.com>
2021-02-09 18:12:04 -0300

    :tada: Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,35 @@
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import Head from 'next/head';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+
+const theme = {
+  colors: {
+    primary: '#0070f3',
+  },
+};
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Clone Instagram</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+}
