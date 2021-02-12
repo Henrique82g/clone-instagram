ce0e6989b57b364af3c319017324c71b31c8561b
Henrique Gonçalves <henriqueco504@gmail.com>
2021-02-12 11:19:08 -0300

    :sparkles: feat: add FollowUser

---
+++ components/FollowUser/index.tsx
@@ -0,0 +1,16 @@
+import { Container } from './styles';
+
+const FollowUser: React.FC = () => {
+  return (
+    <Container>
+      <span className="circle"></span>
+      <div>
+        <span>henriquess</span>
+        <span>Followed by nicolas + 1 more</span>
+      </div>
+      <button type="button">Follow</button>
+    </Container>
+  );
+};
+
+export default FollowUser;
