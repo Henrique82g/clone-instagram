c98cfa5791162ac5de26b52fc4f4fd41ba1f478e
Henrique Gonçalves <henriqueco504@gmail.com>
2021-02-12 09:46:08 -0300

    :sparkles: feat: add Main

---
+++ components/Main/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  max-width: 96.5rem;
+  width: 100%;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+`;
