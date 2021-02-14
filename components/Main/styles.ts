b67c7ab844df8b0ecc18b97b24e6f81f86f3a52f
Henrique Gonçalves <henriqueco504@gmail.com>
2021-02-14 14:06:08 -0300

    :bug: fix: removes the positioning problem

--- components/Main/styles.ts
+++ components/Main/styles.ts
@@ -1,13 +1,17 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div`
-  max-width: 96.5rem;
-  width: 100%;
-  margin: 0 auto;
+export const Wrapper = styled.main`
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-shrink: 0;
 `;
 
-export const Container = styled.div`
-  display: grid;
-  grid-template-columns: 64.4rem 32.1rem;
-  column-gap: 3.3rem;
+export const Container = styled.section`
+  max-width: 96.5rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  justify-content: center;
 `;
