858e12b9e664ea5f3e4f03e6e23b8c79a7ed5e86
Henrique Gonçalves <henriqueco504@gmail.com>
2021-02-17 17:01:55 -0300

    :lipstick: chore: remove height

--- components/FeedItem/styles.ts
+++ components/FeedItem/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   max-width: 64.4rem;
-  height: 63.6rem;
+  /* height: 63.6rem; */
 
   background: var(--color-primary-light);
   border: 0.1rem solid var(--color-border);
