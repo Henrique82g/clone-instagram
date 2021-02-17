470eebc6f4baca6ccf8496ec7efbffee75ff8f93
Henrique Gonçalves <henriqueco504@gmail.com>
2021-02-17 11:12:33 -0300

    :lipstick: chore: update FeedList

--- components/FeedList/styles.ts
+++ components/FeedList/styles.ts
@@ -1,3 +1,11 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+
+  margin: 0 auto;
+  height: 100px;
+  width: 100px;
+  background-color: red;
+`;
