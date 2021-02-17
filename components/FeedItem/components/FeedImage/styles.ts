7d6a9ac863031378194ba40a5fda8344ab622bb0
Henrique Gonçalves <henriqueco504@gmail.com>
2021-02-17 15:19:05 -0300

    :lipstick: chore: add styles

--- components/FeedItem/components/FeedImage/styles.ts
+++ components/FeedItem/components/FeedImage/styles.ts
@@ -1,3 +1,13 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+  height: 34.4rem;
+
+  background: rgba(219, 219, 219, 0.2);
+  filter: brightness(90%);
+
+  :hover {
+    filter: brightness(80%);
+  }
+`;
