21cc972169be7335b6475d53f592197090ec6c0e
Henrique Gonçalves <henriqueco504@gmail.com>
2021-02-17 16:17:16 -0300

    :lipstick: chore: add styles

--- components/FeedItem/components/FeedFooter/index.tsx
+++ components/FeedItem/components/FeedFooter/index.tsx
@@ -7,6 +7,14 @@ function FeedFooter() {
     <Container>
       <FeedActions />
       <span className="view-count">200,000 views</span>
+      <div className="user-comment">
+        <span>henrique</span>
+        <span>Lorem ipsum dolor, sitde</span>
+      </div>
+      <span className="comment-count">View all 1,290 comments</span>
+      <div>
+        <span className="temp">1 hour ago </span>
+      </div>
     </Container>
   );
 }
--- components/FeedItem/components/FeedFooter/styles.ts
+++ components/FeedItem/components/FeedFooter/styles.ts
@@ -1,14 +1,51 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  .view-count {
-    margin-bottom: 0.8rem;
-    font-weight: 500;
+  .view-count,
+  .user-comment {
+    line-height: 1.8rem;
+  }
+
+  .user-comment,
+  .comment-count {
+    margin-bottom: 0.6rem;
+  }
+
+  .view-count,
+  .user-comment,
+  .comment-count {
     font-size: 1.4rem;
+    letter-spacing: -0.165px;
+  }
+
+  .view-count {
+    margin-bottom: 1rem;
+    font-weight: 500;
     line-height: 1.8rem;
 
-    letter-spacing: -0.165px;
-
     color: var(--color-dark);
+  }
+
+  .user-comment {
+    display: flex;
+    gap: 1.1rem;
+
+    span:first-child {
+      font-weight: 500;
+    }
+  }
+
+  div span.temp,
+  .comment-count {
+    color: var(--color-text-light);
+  }
+
+  div span.temp {
+    font-weight: normal;
+    font-size: 1rem;
+    line-height: 1.8rem;
+
+    letter-spacing: 0.2px;
+    text-transform: uppercase;
   }
 `;
