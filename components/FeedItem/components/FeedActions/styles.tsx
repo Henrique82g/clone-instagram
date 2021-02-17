a3e6b82ae1463a1aff1dcc085cf748edd7117d5f
Henrique Gonçalves <henriqueco504@gmail.com>
2021-02-17 15:37:17 -0300

    :lipstick: chore: add styles

--- components/FeedItem/components/FeedActions/styles.tsx
+++ components/FeedItem/components/FeedActions/styles.tsx
@@ -1,3 +1,21 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  vertical-align: middle;
+  margin: 2.5rem 0 0.4rem;
+
+  span:first-child {
+    display: flex;
+    gap: 1.7rem;
+  }
+
+  span svg {
+    width: 2.4rem;
+    height: 2.4rem;
+    color: var(--color-dark);
+    cursor: pointer;
+  }
+`;
