a9f65646c89e056b7d2864c9f435e12e638e6551
Henrique Gonçalves <henriqueco504@gmail.com>
2021-02-17 17:10:10 -0300

    :fire: chore: remove comment

--- components/FeedItem/styles.ts
+++ components/FeedItem/styles.ts
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   max-width: 64.4rem;
-  /* height: 63.6rem; */
 
   background: var(--color-primary-light);
   border: 0.1rem solid var(--color-border);
