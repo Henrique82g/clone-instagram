617e49c674d31bac4fcbcfc6acac2e432c5c6cc6
Henrique Gonçalves <henriqueco504@gmail.com>
2021-02-17 16:30:30 -0300

    :lipstick: chore: add styles

--- components/FeedItem/components/UserComment/styles.ts
+++ components/FeedItem/components/UserComment/styles.ts
@@ -1,3 +1,33 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  cursor: pointer;
+
+  div {
+    display: flex;
+    gap: 1rem;
+
+    span {
+      font-size: 1.4rem;
+      line-height: 1.8rem;
+
+      letter-spacing: -0.165px;
+
+      color: var(--color-dark);
+    }
+
+    span:first-child {
+      font-weight: 500;
+    }
+  }
+
+  svg {
+    width: 1.2rem;
+    height: 1.2rem;
+
+    color: var(--color-dark);
+  }
+`;
